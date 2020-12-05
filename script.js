9554375beed0be64be68ed3ca5fa6aebe9579ab1
BossBin <albin.hos@hotmail.com>
2020-12-05 18:21:36 +0100

    finputs

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
 show_project = function() {
 	document.getElementById("Projects").style.display = "";
+	document.getElementById("ProjectsButton").style.textDecoration  = "underline";	
 	document.getElementById("CV").style.display = "none";
+	document.getElementById("CVButton").style.textDecoration  = "";	
 };
 
 show_cv = function() {
 	document.getElementById("Projects").style.display = "none";
+	document.getElementById("ProjectsButton").style.textDecoration  = "";
 	document.getElementById("CV").style.display = "";
+	document.getElementById("CVButton").style.textDecoration  = "underline";
 };
 
 hideCV = function() {
 	document.getElementById("CV").style.display = "none";
+	document.getElementById("ProjectsButton").style.textDecoration  = "underline";		
 }
 
 window.onload = hideCV;
